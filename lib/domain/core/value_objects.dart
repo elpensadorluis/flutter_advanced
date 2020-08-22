f081311f1a3b47d2a251bb31a604b698af0122b1
Luis Guillermo Echenique <elpensadorluis@gmail.com>
2020-08-22 10:29:32 -0400

    Firebase Auth Setup & Facade

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:dinbog/domain/core/errors.dart';
 import 'package:dinbog/domain/core/failures.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -7,6 +8,12 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+  /// Throws [UnexpectedValueError] containing the [ValueFailure]
+  T getOrCrash() {
+    // id = identity - same as writing (right) => right
+    return value.fold((f) => throw UnexpectedValueError(f), id);
+  }
 
   bool isValid() => value.isRight();
 
---
+++ lib/infrastructure/auth/firebase_auth_facade.dart
@@ -0,0 +1,66 @@
+import 'package:dinbog/domain/auth/auth_failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dinbog/domain/auth/i_auth_facade.dart';
+import 'package:dinbog/domain/auth/value_objects.dart';
+import 'package:dinbog/domain/core/errors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+class FirebaseAuthFacade implements IAuthFacade {
+  final FirebaseAuth _firebaseAuth;
+
+  FirebaseAuthFacade(
+    this._firebaseAuth,
+  );
+
+  @override
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  }) async {
+    final emailAddressStr = emailAddress.getOrCrash();
+    final passwordStr = password.getOrCrash();
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: emailAddressStr,
+        password: passwordStr,
+      );
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        return left(const AuthFailure.emailAlreadyInUse());
+      } else {
+        return left(const AuthFailure.serverError());
+      }
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  }) async {
+    final emailAddressStr = emailAddress.getOrCrash();
+    final passwordStr = password.getOrCrash();
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: emailAddressStr,
+        password: passwordStr,
+      );
+      return right(unit);
+    } on PlatformException catch (e) {
+      if (e.code == 'wrong-password' || e.code == 'user-not-found') {
+        return left(const AuthFailure.invalidEmailAndPasswordCombination());
+      } else {
+        return left(const AuthFailure.serverError());
+      }
+    }
+  }
+
+  @override
+  Future<Either<AuthFailure, Unit>> signInWithFacebook() {
+    // TODO: implement signInWithFacebook
+    throw UnimplementedError();
+  }
+}
