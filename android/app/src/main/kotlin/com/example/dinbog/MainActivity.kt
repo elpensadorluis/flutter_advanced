4d894bdeb3d08d272e350255e2895e0a681c0750
Luis Guillermo Echenique <elpensadorluis@gmail.com>
2020-08-22 09:05:23 -0400

    Punto de control 202008220905

---
+++ android/app/src/main/kotlin/com/example/dinbog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dinbog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:dinbog/domain/auth/auth_failure.dart';
+
+import 'package:dinbog/domain/auth/value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithFacebook();
+}
