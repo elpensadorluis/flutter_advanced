ff9abf4c53aa87f209b1bf0397293f5130a5654b
Luis Guillermo Echenique <elpensadorluis@gmail.com>
2020-08-22 23:45:07 -0400

    Sign-In Form UI Base

---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,23 @@
+import 'package:dinbog/presentation/sign_in/sign_in_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: SignInPage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.blue,
+        accentColor: Colors.blueAccent,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+    );
+  }
+}
